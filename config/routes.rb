9a176c2ff6d5d6c157a79fa89961ffb707a975a7
AlexandreDev06 <alexandre.macedo@ringa.com.br>
2023-11-10 12:12:29 -0300

    Created root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'centrals#index'
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   resources :centrals
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-
-  get 'up' => 'rails/health#show', as: :rails_health_check
 end
